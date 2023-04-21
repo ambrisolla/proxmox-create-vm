ba9219f12b6fa7b8700ef971056f6fea908d618f
ambrisolla <andremuzel@gmail.com>
2023-04-21 09:40:23 -0300

    adding job

--- scripts/01-rundeck-replace-vars.py
+++ scripts/01-rundeck-replace-vars.py
@@ -3,20 +3,6 @@
 import os
 import re
 
-# RD_OPTION_GATEWAY=sd
-# RD_OPTION_HOSTNAME=das
-# RD_OPTION_IPADDR=dsa
-# RD_OPTION_MEMORY=512
-# RD_OPTION_NETMASK=das
-# RD_OPTION_SSH_PASSWORD=das
-# RD_OPTION_SSH_USER=das
-# RD_OPTION_VCPUS=1
-# RD_SECUREOPTION_SSH_PASSWORD=das
-# NETMASK__=255.255.255.0
-# PROXMOX_HOST__=https://proxmox.imbrisa.io:8006/api2/json
-# SALT_MASTER_IP__=192.168.0.201
-# TEMPLATE_IP__=192.168.0.250
-
 env_list = [
     'PROXMOX_HOST',
     'PROXMOX_PASS',
@@ -35,11 +21,12 @@ env_list = [
 ]
 
 
-
 template_vars = open('vars/vm.tfvars', 'r').read()
-
 
 for env in env_list:
     template_vars = re.sub(env,os.environ[env], template_vars)
 
-print(template_vars)
+
+new_template_vars = open('vars/vm.tfvars', 'w')
+new_template_vars.write(template_vars)
+new_template_vars.close()
