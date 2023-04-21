1f646017f8cc9d5b35631e60f41b717468c48775
ambrisolla <andremuzel@gmail.com>
2023-04-21 09:01:52 -0300

    adding job

--- scripts/01-rundeck-replace-vars.py
+++ scripts/01-rundeck-replace-vars.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
-import os
 
+
+# RD_OPTION_GATEWAY=sd
+# RD_OPTION_HOSTNAME=das
+# RD_OPTION_IPADDR=dsa
+# RD_OPTION_MEMORY=512
+# RD_OPTION_NETMASK=das
+# RD_OPTION_SSH_PASSWORD=das
+# RD_OPTION_SSH_USER=das
+# RD_OPTION_VCPUS=1
+# RD_SECUREOPTION_SSH_PASSWORD=das
+# __GATEWAY__=192.168.0.1
+# __HOSTNAME__=teste
+# __IP_ADDR__=192.168.0.222
+# __NETMASK__=255.255.255.0
+# __PROXMOX_HOST__=https://proxmox.imbrisa.io:8006/api2/json
+# __SALT_MASTER_IP__=192.168.0.201
+# __TEMPLATE_IP__=192.168.0.250
+
+
+
+template_vars = open('projects/Proxmox/ScmImpor', 'r').read()
+print(template_vars)
